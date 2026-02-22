1820791a7fffb4738b4099e532480ba63249fdd9
Moin <mointhedev@gmail.com>
2026-02-22 17:15:54 +0500

    Added another simple test

--- test_logic.py
+++ test_logic.py
@@ -1,9 +1,17 @@
-#This is a simptle test script
+# This is a simple test script
 def test_math():
-    assert 2 + 2 == 4 #This should pass
+    assert 2 + 2 == 4  # This should pass
+
 
 def test_string():
     greeting = "Hello to CI/CD"
-    assert "CI/CD" in greeting #This should pass
+    assert "CI/CD" in greeting  # This should pass
+
+
+def test_list():
+    fruits = ["apple", "banana", "cherry"]
+    assert "banana" in fruits
+    assert len(fruits) == 3
+
 
 print("Tests passed successfully!")
